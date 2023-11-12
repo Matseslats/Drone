f88ac161ee24eb8f774221b2b782c65aa384ba5c
Mats <49249061+Matseslats@users.noreply.github.com>
2023-11-12 20:26:25 +0000

    Pin definitions

---
+++ drone_code/drone_code.ino
@@ -0,0 +1,33 @@
+#define LORA_CS A2
+#define LORA_RST A3
+#define BAT_LEVEL A4
+
+#define ESC1 D0
+#define ESC2 D1
+#define ESC3 D2
+#define ESC4 D3
+
+#define SERVO1 D4
+#define SERVO2 D5
+#define SERVO3 D6
+
+#define SPI_CS D7
+#define SPI_MOSI D8
+#define SPI_CK D9
+#define SPI_MISO D10
+
+#define I2C_SDA D11
+#define I2C_SCL D12
+
+#define UART_RX D13
+#define UART_TX D14
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
