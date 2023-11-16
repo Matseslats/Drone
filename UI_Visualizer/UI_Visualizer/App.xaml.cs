4ba4c02fc94c3bae0030b881e10620aba8800eb1
Mats <49249061+Matseslats@users.noreply.github.com>
2023-11-16 21:48:24 +0000

    Added 3D map to UI visualizer

---
+++ UI_Visualizer/UI_Visualizer/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Windows;
+
+namespace UI_Visualizer;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        string apiKeyVarName = "ARCGIS_API_KEY";
+
+        try
+        {
+            string apiKey = EnvironmentVariableReader.GetEnvironmentVariable(apiKeyVarName);
+            // Note: it is not best practice to store API keys in source code.
+            // The API key is referenced here for the convenience of this tutorial.
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = apiKey;
+            Debug.WriteLine("Aquired ArcGIS API KEY");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Debug.WriteLine(ex.Message);
+        }
+    }
+}
+
---
+++ UI_Visualizer/UI_Visualizer/EnvironmentVariableReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace UI_Visualizer;
+
+public class EnvironmentVariableReader
+{
+    public static string GetEnvironmentVariable(string variableName)
+    {
+        string variableValue = Environment.GetEnvironmentVariable(variableName);
+
+        if (variableValue != null)
+        {
+            return variableValue;
+        }
+        else
+        {
+            // You can throw an exception or handle the case as appropriate for your application
+            throw new InvalidOperationException($"The environment variable '{variableName}' is not set.");
+        }
+    }
+}
